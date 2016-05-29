094d72e62cdde588979c750a7af1065c0606bc0d
Daniel McPherson <Daniel.Mcpherson@RapidCircle.com>
2016-05-29 15:08:46 +0200

    Initial Commit

---
+++ DefaultValuesWeb/RemoteEventReceiverManager.cs
@@ -0,0 +1,233 @@
+﻿using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.EventReceivers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace DefaultValuesWeb.Services
+{
+    public class RemoteEventReceiverManager
+    {
+        private const string RECEIVER_NAME_ADDED = "DefaultValues";
+        private const string LIST_TITLE = "documents";
+
+
+        public void ItemUpdatedToListEventHandler(ClientContext clientContext, Guid listId, int listItemId)
+        {
+            try
+            {
+                UpdateDefaultValues(clientContext, listId, listItemId);
+            }
+            catch (Exception oops)
+            {
+                System.Diagnostics.Trace.WriteLine(oops.Message);
+            }
+        }
+
+        public void ItemAddingToListEventHandler(SPRemoteEventProperties properties, SPRemoteEventResult result)
+        {
+            try
+            {
+                UpdateDefaultValuesAdding(properties, result);
+            }
+            catch (Exception oops)
+            {
+                System.Diagnostics.Trace.WriteLine(oops.Message);
+            }
+        }
+
+        public void AssociateRemoteEventsToHostWeb(ClientContext clientContext)
+        {
+            var rerList = clientContext.Web.Lists.GetByTitle(LIST_TITLE);
+            clientContext.Load(rerList);
+            clientContext.ExecuteQuery();
+
+            bool rerExists = false;
+            if (!rerExists)
+            {
+                //Get WCF URL where this message was handled
+                OperationContext op = OperationContext.Current;
+                Message msg = op.RequestContext.RequestMessage;
+
+                EventReceiverDefinitionCreationInformation receiver = new EventReceiverDefinitionCreationInformation();
+
+                receiver.EventType = EventReceiverType.ItemAdding;
+                receiver.ReceiverUrl = msg.Headers.To.ToString();
+                receiver.ReceiverName = EventReceiverType.ItemAdding.ToString();
+                receiver.Synchronization = EventReceiverSynchronization.Synchronous;
+                receiver.SequenceNumber = 350;
+                rerList.EventReceivers.Add(receiver);
+                clientContext.ExecuteQuery();
+                System.Diagnostics.Trace.WriteLine("Added ItemAdded receiver at " + receiver.ReceiverUrl);
+
+                receiver = new EventReceiverDefinitionCreationInformation();
+                receiver.EventType = EventReceiverType.ItemUpdated;
+                receiver.ReceiverUrl = msg.Headers.To.ToString();
+                receiver.ReceiverName = EventReceiverType.ItemUpdated.ToString();
+                receiver.Synchronization = EventReceiverSynchronization.Synchronous;
+                receiver.SequenceNumber = 350;
+                rerList.EventReceivers.Add(receiver);
+                clientContext.ExecuteQuery();
+
+                System.Diagnostics.Trace.WriteLine("Added ItemUpdated receiver at " + receiver.ReceiverUrl);
+            }
+        }
+
+        public void RemoveEventReceiversFromHostWeb(ClientContext clientContext)
+        {
+            List myList = clientContext.Web.Lists.GetByTitle(LIST_TITLE);
+            clientContext.Load(myList, p => p.EventReceivers);
+            clientContext.ExecuteQuery();
+
+            var rer = myList.EventReceivers.Where(e => e.ReceiverName == RECEIVER_NAME_ADDED).FirstOrDefault();
+
+            try
+            {
+                System.Diagnostics.Trace.WriteLine("Removing receiver at "
+                        + rer.ReceiverUrl);
+
+                var rerList = myList.EventReceivers.Where(e => e.ReceiverUrl == rer.ReceiverUrl).ToList<EventReceiverDefinition>();
+
+                foreach (var rerFromUrl in rerList)
+                {
+                    //This will fail when deploying via F5, but works
+                    //when deployed to production
+                    rerFromUrl.DeleteObject();
+                }
+                clientContext.ExecuteQuery();
+            }
+            catch (Exception oops)
+            {
+                System.Diagnostics.Trace.WriteLine(oops.Message);
+            }
+
+            clientContext.ExecuteQuery();
+        }
+
+        ///// <summary>
+        ///// Handles the ItemAdded event by modifying the Description
+        ///// field of the item.
+        ///// </summary>
+        ///// <param name="properties"></param>
+        private void UpdateDefaultValuesAdding(SPRemoteEventProperties properties, SPRemoteEventResult result)
+        {
+            using (ClientContext clientContext = TokenHelper.CreateRemoteEventReceiverClientContext(properties))
+            {
+                if (clientContext != null)
+                {
+                    var itemProperties = properties.ItemEventProperties;
+                    var _userLoginName = properties.ItemEventProperties.UserLoginName;
+                    var _afterProperites = itemProperties.AfterProperties;
+                    string phase = "";
+
+                    List list = clientContext.Web.Lists.GetById(properties.ItemEventProperties.ListId);
+                    //ListItem item = list.GetItemById(properties.ItemEventProperties.ListItemId);
+                    FieldCollection fields = list.Fields;
+                    //clientContext.Load(item);
+                    clientContext.Load(fields);
+                    clientContext.ExecuteQuery();
+
+                    string[] folderNameSplit = itemProperties.BeforeUrl.ToString().Split('/');
+                    string folderName = folderNameSplit[folderNameSplit.Length - 2];
+                    string code = folderName.Split('_').First();
+
+                    string lookupFieldName = "Phase";
+                    FieldLookup lookupField = clientContext.CastTo<FieldLookup>(fields.GetByInternalNameOrTitle(lookupFieldName));
+                    clientContext.Load(lookupField);
+                    clientContext.ExecuteQuery();
+
+                    Guid parentWeb = lookupField.LookupWebId;
+                    string parentListGUID = lookupField.LookupList;
+
+                    Web lookupListWeb = clientContext.Site.OpenWebById(parentWeb);
+                    List parentList = lookupListWeb.Lists.GetById(new Guid(parentListGUID));
+
+                    CamlQuery cq = new CamlQuery();
+                    cq.ViewXml = "<ViewFields><FieldRef Name='Title' /><FieldRef Name='ID' /></ViewFields>";
+
+                    ListItemCollection litems = parentList.GetItems(cq);
+                    clientContext.Load(parentList);
+                    clientContext.Load(litems);
+                    clientContext.ExecuteQuery();
+
+                    FieldLookupValue flv = new FieldLookupValue();
+                    foreach (ListItem li in litems)
+                    {
+                        string phaseItemCode = li["Title"].ToString().TrimEnd(')').Split('(').Last();
+                        if (phaseItemCode == code)
+                        {
+                            if (itemProperties.AfterProperties.ContainsKey("Phase") && !String.IsNullOrEmpty(itemProperties.AfterProperties["Phase"].ToString()))
+                            {
+                                string[] stringSeparators = new string[] { ";#" };
+                                if (itemProperties.AfterProperties["Phase"].ToString().Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries)[0] == li["ID"].ToString()) break;
+                            }
+
+                            phase = li.Id + ";#" + li["Title"];
+                            break;
+                        }
+                    }
+
+                    result.ChangedItemProperties.Add("Phase", phase);
+                    result.Status = SPRemoteEventServiceStatus.Continue;
+                }
+            }
+        }
+
+        public void UpdateDefaultValues(ClientContext clientContext, Guid listId, int listItemId)
+        {
+            List list = clientContext.Web.Lists.GetById(listId);
+            ListItem item = list.GetItemById(listItemId);
+            FieldCollection fields = list.Fields;
+            clientContext.Load(item);
+            clientContext.Load(fields);
+            clientContext.ExecuteQuery();
+
+            if (item["Phase"] != null && !string.IsNullOrEmpty(item["Phase"].ToString()))
+                return;
+
+            string folderName = item["FileDirRef"].ToString().Split('/').Last();
+            string code = folderName.Split('_').First();
+
+            string lookupFieldName = "Phase";
+            FieldLookup lookupField = clientContext.CastTo<FieldLookup>(fields.GetByInternalNameOrTitle(lookupFieldName));
+            clientContext.Load(lookupField);
+            clientContext.ExecuteQuery();
+
+            Guid parentWeb = lookupField.LookupWebId;
+            string parentListGUID = lookupField.LookupList;
+
+            Web lookupListWeb = clientContext.Site.OpenWebById(parentWeb);
+            List parentList = lookupListWeb.Lists.GetById(new Guid(parentListGUID));
+
+            CamlQuery cq = new CamlQuery();
+            cq.ViewXml = "<ViewFields><FieldRef Name='Title' /><FieldRef Name='ID' /></ViewFields>";
+
+            ListItemCollection litems = parentList.GetItems(cq);
+            clientContext.Load(parentList);
+            clientContext.Load(litems);
+            clientContext.ExecuteQuery();
+
+            foreach (ListItem li in litems)
+            {
+                string phaseItemCode = li["Title"].ToString().TrimEnd(')').Split('(').Last();
+                if (phaseItemCode == code)
+                {
+                    string value = item.Id + ";#" + item["Title"];
+                    if (item["Phase"] != null)
+                    {
+                        FieldLookupValue flv = (FieldLookupValue)item["Phase"];
+                        string existingCode = flv.LookupValue.ToString().TrimEnd(')').Split('(').Last();
+                        if (existingCode == code) break;
+                    }
+
+                    item["Phase"] = li.Id + ";#" + li["Title"];
+                    item.Update();
+                    clientContext.ExecuteQuery();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ DefaultValuesWeb/Services/AppEventReceiver.svc.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.EventReceivers;
+using System.Runtime.Caching;
+
+namespace DefaultValuesWeb.Services
+{
+    public class AppEventReceiver : IRemoteEventService
+    {
+
+        //the cache needs an unique name
+        private static MemoryCache _cacheObject = new MemoryCache("26397979caf840b39c36368fa9b1bb9f");
+
+        /// <summary>
+        /// Handles app events that occur after the app is installed or upgraded, or when app is being uninstalled.
+        /// </summary>
+        /// <param name="properties">Holds information about the app event.</param>
+        /// <returns>Holds information returned from the app event.</returns>
+        public SPRemoteEventResult ProcessEvent(SPRemoteEventProperties properties)
+        {
+            SPRemoteEventResult _result = new SPRemoteEventResult();
+
+            try
+            {
+                switch (properties.EventType)
+                {
+                    case SPRemoteEventType.AppInstalled:
+                        HandleAppInstalled(properties);
+                        break;
+                    case SPRemoteEventType.AppUninstalling:
+                        HandleAppUninstalling(properties);
+                        break;
+                    case SPRemoteEventType.ItemAdding:
+                        new RemoteEventReceiverManager().ItemAddingToListEventHandler(properties, _result);
+                        break;
+                    case SPRemoteEventType.ItemUpdated:
+                        HandleItemUpdated(properties);
+                        break;
+                }
+                _result.Status = SPRemoteEventServiceStatus.Continue;
+            }
+            catch (Exception ex)
+            {
+                //You should log here.               
+            }
+            return _result;
+        }
+
+        /// <summary>
+        /// Handles the ItemUpdating event by check the Description
+        /// field of the item.
+        /// </summary>
+        /// <param name="properties"></param>
+        private void HandleItemUpdated(SPRemoteEventProperties properties)
+        {
+            using (ClientContext clientContext = TokenHelper.CreateRemoteEventReceiverClientContext(properties))
+            {
+                if (clientContext != null)// && EventFiringEnabled(clientContext, properties, "2"))
+                {
+                    new RemoteEventReceiverManager().ItemUpdatedToListEventHandler(clientContext, properties.ItemEventProperties.ListId, properties.ItemEventProperties.ListItemId);
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method is a required placeholder, but is not used by app events.
+        /// </summary>
+        /// <param name="properties">Unused.</param>
+        public void ProcessOneWayEvent(SPRemoteEventProperties properties)
+        {
+        }
+
+        /// <summary>
+        /// Handles when an app is installed.  Activates a feature in the
+        /// host web.  The feature is not required.  
+        /// Next, if the Jobs list is
+        /// not present, creates it.  Finally it attaches a remote event
+        /// receiver to the list.  
+        /// </summary>
+        /// <param name="properties"></param>
+        private void HandleAppInstalled(SPRemoteEventProperties properties)
+        {
+            using (ClientContext clientContext = TokenHelper.CreateAppEventClientContext(properties, false))
+            {
+                if (clientContext != null)
+                {
+                    new RemoteEventReceiverManager().AssociateRemoteEventsToHostWeb(clientContext);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes the remote event receiver from the list and 
+        /// adds a new item to the list.
+        /// </summary>
+        /// <param name="properties"></param>
+        private void HandleAppUninstalling(SPRemoteEventProperties properties)
+        {
+            using (ClientContext clientContext =
+                TokenHelper.CreateAppEventClientContext(properties, false))
+            {
+                if (clientContext != null)
+                {
+                    new RemoteEventReceiverManager().RemoveEventReceiversFromHostWeb(clientContext);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Used to chek if the event should processed or not. we use here a local MemoryCache object 
+        /// or if the RER is loadbalanced a Redis Server on AZURE or onPrem
+        /// </summary>
+        /// <param name="ctx">the current context as ClientContext</param>
+        /// <param name="properties">the event properies as SPRemoteEventProperties</param>
+        /// <param name="eventType">identifier for event receiver type (~ing=1, ~ed=2) as String</param>
+        /// <returns></returns>
+        private static bool EventFiringEnabled(ClientContext ctx, SPRemoteEventProperties properties, string eventType)
+        {
+            try
+            {
+                // set the correlation id for the next roundtrip to context
+                ctx.TraceCorrelationId = properties.CorrelationId.ToString();
+
+                var key = string.Concat(properties.CorrelationId.ToString("N"), eventType);
+                var ts = TimeSpan.FromSeconds(20);
+
+                return _cacheObject.Add(key, properties.CorrelationId, new CacheItemPolicy
+                {
+                    Priority = CacheItemPriority.Default,
+                    SlidingExpiration = ts
+                });
+            }
+            catch (Exception oops)
+            {
+                System.Diagnostics.Trace.WriteLine(oops.Message);
+            }
+
+            return false;
+        }
+
+    }
+}
